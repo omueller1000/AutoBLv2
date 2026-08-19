ae937110c6748fb36c7fffc0fbe816d101e564cc
Oliver Mueller <omueller@slac.stanford.edu>
2026-08-18 23:08:26 -0700

    started to implement AutoGain

--- AutoBLv2/Automation/ToDo/AutoGain.cs
+++ AutoBLv2/Automation/ToDo/AutoGain.cs
@@ -1,4 +1,5 @@
 ﻿using Devices;
+using FPGA;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,19 +16,160 @@ namespace Automation
         public const Int32 ERROR = -1;
         public const Int32 ABORT = 0;
         //-----------------------------------------------------------
+        private const Int32 SRS_SLEEP = 100;
+        //-----------------------------------------------------------
         #endregion
 
 
 
 
 
-        public Int32 FindMaxGain(ref SRS570[] _amplifiers, ref string _error)
+        #region Properties
+        //-----------------------------------------------------------
+        public bool Abort { get; set; }
+        //-----------------------------------------------------------
+        #endregion
+
+
+
+
+        //===========================================================
+        public AutoGain()
+        {
+            this.Abort = false;            
+        }
+        //===========================================================
+
+
+
+
+
+
+        public Int32 FindMaxGain(ref SRS570[] _amplifiers, out Int32[] _sensId, ref string _error)
         {
             Int32 rt;
-            Int32 minSensId = 21;
-            Int32 maxSensId = 12;
+            Int32 minSensId = 21;   // 1e5
+            Int32 maxSensId = 9;   // 1e9
+
+            double[] aiAverage;
+            double[] aiStdDev;
+            
+            bool[] findMaxGainDone;
+            bool allFindMaxGainDone = false;
 
 
+
+            _sensId = new Int32[_amplifiers.Length];
+            findMaxGainDone = new bool[_amplifiers.Length];
+            for (Int32 amplifierIdx = 0; amplifierIdx < _amplifiers.Length; amplifierIdx++)
+            {
+                // reset Offset
+                rt = _amplifiers[amplifierIdx].SetOffset(0, 1, ref _error);
+
+                findMaxGainDone[amplifierIdx] = false;
+                _sensId[amplifierIdx] = minSensId;
+            }
+
+
+            // runs until all amplfiers have been optimized
+            while (true)
+            {
+                // set gain
+                for (Int32 amplifierIdx = 0; amplifierIdx < _amplifiers.Length; amplifierIdx++)
+                {
+                    if (findMaxGainDone[amplifierIdx])
+                        continue;
+                    
+                    rt = _amplifiers[amplifierIdx].SetGain(_sensId[amplifierIdx], ref _error);
+                    if (rt != SRS570.SUCCESS)
+                    {
+                        _error = this.GetType().Name + " " + System.Reflection.MethodBase.GetCurrentMethod().Name + " " + _error;
+                        return ERROR;
+                    }                    
+                }
+
+
+                Thread.Sleep(SRS_SLEEP); // wait after swiching the gain
+
+
+                // collect data
+                // expects all amplifiers to be connnected to the same DAQ !!!!!!!!
+                rt = _amplifiers[0].Daq.CollectDataCalibrated(out aiAverage, out aiStdDev);
+                if (rt != FpgaDaq.SUCCESS)
+                {
+                    _error = this.GetType().Name + " " + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                    _error += " CollectDataCalibrated";
+                    return ERROR;
+                }
+
+
+                // adjust gains
+                for (Int32 amplifierIdx = 0; amplifierIdx < _amplifiers.Length; amplifierIdx++)
+                {
+                    if (!findMaxGainDone[amplifierIdx])
+                        continue;
+                    
+                    if (aiAverage[_amplifiers[amplifierIdx].AiChannel] > _amplifiers[amplifierIdx].SignalHighLimit)
+                    {
+                        // if gain was too high, reduce gain and mark this amplifier as optimized
+                        _sensId[amplifierIdx]++; // decrease gain
+                        findMaxGainDone[amplifierIdx] = true;
+                    }
+                    else if (_sensId[amplifierIdx] == maxSensId)
+                    {
+                        // if gain is already set to max gain, mark it as optimized
+                        findMaxGainDone[amplifierIdx] = true;
+                    }
+                    else
+                    {
+                        // if gain is still too low increase gain but not above max gain
+                        if (_sensId[amplifierIdx] > maxSensId) // make sure not too exceed max gain
+                            _sensId[amplifierIdx]--; // increase gain
+                    }
+                    
+                }
+
+
+                // check if all amplifiers have been optimized
+                allFindMaxGainDone = true;
+                for (Int32 amplifierIdx = 0; amplifierIdx < _amplifiers.Length; amplifierIdx++)
+                    allFindMaxGainDone &= findMaxGainDone[amplifierIdx];
+
+                if (allFindMaxGainDone)
+                    break;
+
+
+                if (this.Abort)
+                    break;
+
+                Console.Write(".");
+
+            }
+
+
+
+            // finally
+            for (Int32 amplifierIdx = 0; amplifierIdx < _amplifiers.Length; amplifierIdx++)
+            {
+                // set found gain
+                rt = _amplifiers[amplifierIdx].SetGain(_sensId[amplifierIdx], ref _error);
+                if (rt != SRS570.SUCCESS)
+                {
+                    _error = this.GetType().Name + " " + System.Reflection.MethodBase.GetCurrentMethod().Name + " " + _error;
+                    return ERROR;
+                }
+            }
+
+
+
+            if (this.Abort)
+            {
+                Console.WriteLine("FindMaxGain: ABORT");
+                this.Abort = false; // reset ABORT
+                return ABORT;
+            }
+
+            Console.WriteLine("FindMaxGain: SUCCESS");
 
             return SUCCESS;
         }
--- AutoBLv2/SRV/BlSrv.cs
+++ AutoBLv2/SRV/BlSrv.cs
@@ -21,6 +21,18 @@ namespace AutoBLv2.SRV
         //-----------------------------------------------------------
         #endregion
 
+        private Int32 REQUEST_LENGTH_AUTO_GAIN_MIN = 4;
+        private Int32 REQUEST_LENGTH_AUTO_GAIN_MAX = 12;
+        private Int32 OFFSET_AUTO_GAIN_EXECUTE = 2;
+
+        private Int32 MAX_AMPLIFIER_INDEX = 4;  // beamline specific
+        private Int32 MAX_AMPLIFIER_COUNT = 6;
+        private Int32 MAX_ENERGY_COUNT = 6;
+        private Int32 MIN_MONO_ENERGY = 5000;
+        private Int32 MAX_MONO_ENERGY = 40000;
+
+
+
 
 
         #region Variables
@@ -30,7 +42,15 @@ namespace AutoBLv2.SRV
         //-----------------------------------------------------------
         private static readonly object __taskLock = new object();
         private static string __currentTask = "";
+        private static string __staticError = "";
+        private Thread __WorkerThread;
         private Thread __MonitorThread;
+        //-----------------------------------------------------------
+        private SRS570 __srsI0;
+        private SRS570 __srsI1;
+        private SRS570 __srsI2;
+        private SRS570 __srsI3;
+        private SRS570[] __srsArr;
         //-----------------------------------------------------------
         #endregion
 
@@ -41,6 +61,12 @@ namespace AutoBLv2.SRV
         {
             __fpgaDaq = _fpgaDaq;
             __serverLock = _lock;
+
+            __srsI0 = new SRS570("BL22:SRS570_AMP1", ref __fpgaDaq, 0);
+            __srsI1 = new SRS570("BL22:SRS570_AMP2", ref __fpgaDaq, 1);
+            __srsI2 = new SRS570("BL22:SRS570_AMP3", ref __fpgaDaq, 2);
+            __srsI3 = new SRS570("BL22:SRS570_AMP4", ref __fpgaDaq, 3);
+            __srsArr = new SRS570[] { __srsI0, __srsI1, __srsI2, __srsI3 };
 
         }
         //===========================================================
@@ -77,13 +103,7 @@ namespace AutoBLv2.SRV
 
 
 
-        private Int32 REQUEST_LENGTH_AUTO_GAIN_MIN = 4;
-        private Int32 REQUEST_LENGTH_AUTO_GAIN_MAX = 12;
-        private Int32 OFFSET_AUTO_GAIN_EXECUTE = 2;
 
-        private Int32 MAX_AMPLIFIER_INDEX = 4;  // beamline specific
-        private Int32 MIN_MONO_ENERGY = 5000;
-        private Int32 MAX_MONO_ENERGY = 40000;
 
 
         private Int32 CmdAutoGain(ref string[] _reqArr, ref string _res, ref string _error)
@@ -109,6 +129,8 @@ namespace AutoBLv2.SRV
             ampliferIndexList = new List<Int32>();
             energyList = new List<double>();
 
+
+
             // parse aguments
             try
             {
@@ -116,11 +138,11 @@ namespace AutoBLv2.SRV
 
                 for (Int32 i = OFFSET_AUTO_GAIN_EXECUTE + 1; i < _reqArr.Length; i++)
                 {
-                    if (_reqArr[i][0] == 'A')
+                    if (_reqArr[i][0] == 'A' || _reqArr[i][0] == 'a')
                     {
                         ampliferIndexList.Add(Int32.Parse(_reqArr[i].Substring(1)));
                     }
-                    else if (_reqArr[i][0] == 'E')
+                    else if (_reqArr[i][0] == 'E' || _reqArr[i][0] == 'e')
                     {
                         energyList.Add(double.Parse(_reqArr[i].Substring(1)));
                     }
@@ -131,8 +153,6 @@ namespace AutoBLv2.SRV
                         return ERROR;
                     }
                 }
-
-
             }
             catch (Exception ex)
             {
@@ -141,9 +161,17 @@ namespace AutoBLv2.SRV
             }
 
 
+            
             // verify arguments
             try
             {
+                if (ampliferIndexList.Count > MAX_AMPLIFIER_COUNT)
+                {
+                    _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                    _error += " TOO_MANY_AMPLIFIERS";
+                    return ERROR;
+                }                
+
                 for (Int32 i = 0; i < ampliferIndexList.Count; i++)
                 {
                     if (ampliferIndexList[i] < 0 || ampliferIndexList[i] > MAX_AMPLIFIER_INDEX)
@@ -152,6 +180,25 @@ namespace AutoBLv2.SRV
                         _error += " ARGUMENT_OUT_OF_RANGE";
                         return ERROR;
                     }
+
+                    // search dublicates
+                    for (Int32 j = 0; j < i; j++)
+                    {
+                        if (ampliferIndexList[i] == ampliferIndexList[j])
+                        {
+                            _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                            _error += " DUPLICATE_ARGUMENT";
+                            return ERROR;
+                        }
+                    }
+                }
+
+
+                if (energyList.Count > MAX_ENERGY_COUNT)
+                {
+                    _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                    _error += " TOO_MANY_ENERGIES";
+                    return ERROR;
                 }
 
                 for (Int32 i = 0; i < energyList.Count; i++)
@@ -161,6 +208,17 @@ namespace AutoBLv2.SRV
                         _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
                         _error += " ARGUMENT_OUT_OF_RANGE";
                         return ERROR;
+                    }
+
+                    // search dublicates
+                    for (Int32 j = 0; j < i; j++)
+                    {
+                        if (energyList[i] == energyList[j])
+                        {
+                            _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                            _error += " DUPLICATE_ARGUMENT";
+                            return ERROR;
+                        }
                     }
                 }
             }
@@ -175,10 +233,81 @@ namespace AutoBLv2.SRV
                 return SUCCESS;
 
 
+
+
+            if (__WorkerThread != null && __WorkerThread.IsAlive)
+            {
+                _error = this.GetType().Name + "." + System.Reflection.MethodBase.GetCurrentMethod().Name;
+                _error += " THREAD_ALIVE";
+                return ERROR;
+            }
+
+
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            lock (__taskLock)
+            {
+                __currentTask = $"AUTO_GAIN";
+            }
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+            ampliferIndexList.Sort();
+            energyList.Sort();
+
+
+            __WorkerThread = new Thread(() => AutoGainBlocking(ampliferIndexList, energyList, ref __staticError));
+            __WorkerThread.IsBackground = true;
+            __WorkerThread.Start();
+
+            __MonitorThread = new Thread(() => MonitorThread(ref __WorkerThread, ""));
+            __MonitorThread.IsBackground = true;
+            __MonitorThread.Start();
+
+
+
             return SUCCESS;
         }
         //-----------------------------------------------------------
         #endregion
+
+
+
+
+
+
+        private Int32 AutoGainBlocking(List<Int32> _amplifiers, List<double> _energies, ref string _error)
+        {
+            Int32 rt;
+            AutoGain AutoGain;
+            SRS570[] selectedAmplifiers;
+            Int32[] sensId;
+
+            
+
+            selectedAmplifiers = new SRS570[_amplifiers.Count];
+            for (Int32 i = 0; i < _amplifiers.Count; i++)
+            {
+                selectedAmplifiers[i] = __srsArr[_amplifiers[i]];
+            }
+
+
+
+            AutoGain = new AutoGain();
+            AutoGain.FindMaxGain(ref selectedAmplifiers, out sensId, ref _error);
+
+
+
+
+            return SUCCESS;
+        }
+
+
+
+
+
+
 
 
 
